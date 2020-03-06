2b843abf1c9086c9116ea57af17cc322eb21a90b
Scott <dscottwiggins@gmail.com>
2020-03-06 09:20:09 -0500

    add url to test config

--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,7 @@ import Config
 # you can enable the server option below.
 config :dnd, DndWeb.Endpoint,
   http: [port: 4002],
+  url: [host: "localhost", port: 80],
   server: true
 
 # Print only warnings and errors during test
