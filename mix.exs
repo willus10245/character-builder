4b48d4d3e946ca30dd9e33533555a4e8ae28bb89
Scott <dscottwiggins@gmail.com>
2020-04-30 09:01:23 -0400

    everybody do the dependency dance

--- mix.exs
+++ mix.exs
@@ -34,11 +34,12 @@ defmodule Dnd.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.1"},
-      {:phoenix_pubsub, "~> 2.0"},
-      {:phoenix_html, "~> 2.14"},
-      {:phoenix_live_dashboard, "~> 0.1"},
+      {:phoenix_html, "~> 2.14.1"},
+      {:phoenix_live_dashboard, "~> 0.2.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.12.1"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
